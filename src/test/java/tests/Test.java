ad04fad0eeec8f9022e5975b1f704718071d94e7
Oleg <prostodust@gmail.com>
2021-05-02 13:57:33 +0300

    Update

---
+++ src/test/java/tests/Test.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+public class Test {
+
+// IE
+//        System.setProperty("webdriver.ie.driver", "src/test/resources/IEDriverServer.exe");
+//        WebDriver driver = new InternetExplorerDriver();
+// Edge
+//        System.setProperty("webdriver.edge.driver", "src/test/resources/msedgedriver.exe");
+//        EdgeDriver driver = new EdgeDriver();
+// Yandex
+//        System.setProperty("webdriver.chrome.driver", "src/test/resources/yandexdriver.exe");
+//        WebDriver driver = new ChromeDriver();
+// Chrome
+//        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+}
